01b1c756e35f2c6eb8b0e4875a187e48b962a023
nicolezhu <nzhu94@gmail.com>
2015-04-15 19:47:48 -0500

    first commit

---
+++ BiteNowControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BiteNowControl
+//
+//  Created by Nicole Zhu on 4/15/15.
+//  Copyright (c) 2015 Delta Hackers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
